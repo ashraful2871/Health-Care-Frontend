e73b10240ef21fb35b188620e598a2310f84df94
ashraful2871 <ashrafulislam2758@gmail.com>
2025-11-08 15:19:22 +0600

    add proxy file and match route

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,54 +1,22 @@
-import next from "next";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtDecode } from "jwt-decode";
 
-interface UserInterface {
-  id: string;
-  email: string;
-  role: "ADMIN" | "PATIENT" | "DOCTOR";
-  exp: number;
-  lat: number;
-}
-
-const roleBasedRoutes = {
-  ADMIN: ["/admin/dashboard"],
-  DOCTOR: ["/doctor/dashboard"],
-  PATIENT: [
-    "/patient/dashboard",
-    "/patient/appointments",
-    "/patient/medical-records",
-  ],
-};
-
-const authRoutes = ["/login", "/register", "/forgot-password"];
-export async function proxy(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
-  const { pathname } = request.nextUrl;
-
-  if (!accessToken && !refreshToken && !authRoutes.includes(pathname)) {
-    return NextResponse.redirect(
-      new URL(`/login?redirect=${pathname}`, request.url)
-    );
-  }
-  let user: UserInterface | null = null;
-  if (accessToken) {
-    try {
-      user = jwtDecode(accessToken);
-      console.log(user);
-    } catch (error) {
-      console.log(error);
-      return NextResponse.redirect(
-        new URL(`/login?redirect=${pathname}`, request.url)
-      );
-    }
-  }
-
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  console.log("pathname", request.nextUrl.pathname);
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/dashboard/:path*", "/login", "/register", "/forgot-password"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt.well-known).*)",
+  ],
 };
