523a8824238d4c6ada2078b8371151a90f12dd8c
ashraful2871 <ashrafulislam2758@gmail.com>
2025-11-11 23:12:27 +0600

    make dashboard layout

---
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardNavbar = () => {
+  return (
+    <div>
+      <h1>DashboardNavbar.tsx</h1>
+    </div>
+  );
+};
+
+export default DashboardNavbar;
---
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardSidebar = () => {
+  return (
+    <div>
+      <h1>DashboardSidebar.tsx</h1>
+    </div>
+  );
+};
+
+export default DashboardSidebar;
