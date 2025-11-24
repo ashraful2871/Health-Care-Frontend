a71cbd2795207916f1caa56fec97a96de80f8c31
ashraful2871 <ashrafulislam2758@gmail.com>
2025-11-24 22:31:36 +0600

    Reset Password Pages For Admin and Doctor Role

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -5,15 +5,10 @@ export type RouteConfig = {
   pattern: RegExp[];
 };
 
-export const authRoutes = [
-  "/login",
-  "/register",
-  "/forgot-password",
-  "/reset-password",
-];
+export const authRoutes = ["/login", "/register", "/forgot-password"];
 
 export const commonProtectedRoutes: RouteConfig = {
-  exact: ["/my-profile", "/settings", "/change-password"],
+  exact: ["/my-profile", "/settings", "/change-password", "/reset-password"],
   pattern: [],
 };
 
